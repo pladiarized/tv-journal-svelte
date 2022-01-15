2217cfb8187e32d694b24e74fb854e1284276073
vaggy <sarvagya6@gmail.com>
2022-01-14 21:50:25 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import ShowInput from "./ShowInput.svelte";
+
+	const submitShow = show => {
+		console.log(show);
+	};
+</script>
+
+<div class="main">
+	<h1>
+		TV Journal
+	</h1>
+
+	<ShowInput on:submitShow={
+		e => submitShow(e.detail.show)
+	} />
+</div>
+
+<style>
+
+	.main {
+		max-width: 500px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+</style>
---
+++ src/ShowInput.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    let title = '';
+    let rating = 1;
+
+    const onTitleChange = (e) => {
+        title = e.target.value;
+    }
+
+    const onRatingSelect = (e) => {
+        rating = e.target.value;
+    }
+
+    const submitShow = () => {
+        if (title.length > 0) {
+            dispatch('submitShow', { show: { title, rating } });
+
+            title = '';
+            rating = 1;
+        }
+    }
+
+</script>
+
+<input 
+    type="text" 
+    placeholder="Show Title"
+    value={title}
+    on:keyup={onTitleChange}
+/>
+
+<select value={rating} on:change={onRatingSelect}>
+    <option value={1}>1</option>
+    <option value={2}>2</option>
+    <option value={3}>3</option>
+    <option value={4}>4</option>
+    <option value={5}>5</option>
+</select>
+
+<button disabled={!title} on:click={submitShow}>
+    Submit
+</button>
